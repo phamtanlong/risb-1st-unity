31fa657f022c9130d7b8ae844764db598db010df
biennv.uit@gmail.com <biennv.uit@gmail.com@4b86bf70-225a-8a65-f651-2f2a76d954b9>
2013-08-04 11:12:41 +0000

    (+) fix

--- trunk/New Unity Project 1/Assets/Scripts/menu.cs
+++ trunk/New Unity Project 1/Assets/Scripts/menu.cs
@@ -24,27 +24,27 @@ public class menu : MonoBehaviour
 
 		// Tổng thể
 		if (GUI.Button (new Rect (20, 140, 150, 20), "Tổng thể")) {
-			Application.LoadLevel (1);
+			
 		}
 
 		// Mặt trước
 		if (GUI.Button (new Rect (20, 165, 150, 20), "Mặt trước")) {
-			Application.LoadLevel (2);
+			
 		}
 		
 		//Mặt trong
 		if (GUI.Button (new Rect (20, 190, 150, 20), "Mặt trong")) {
-			Application.LoadLevel (3);
+			
 		}
 		
 		//Khung tủ
 		if (GUI.Button (new Rect (20, 215, 150, 20), "Khung tủ")) {
-			Application.LoadLevel (4);
+			
 		}
 		
 		//Mặt sau
 		if (GUI.Button (new Rect (20, 240, 150, 20), "Mặt sau")) {
-			Application.LoadLevel (5);
+			
 		}
 		#endregion
 		
@@ -58,27 +58,27 @@ public class menu : MonoBehaviour
 		
 		//Bảng mach CPU
 		if (GUI.Button (new Rect (20, 305, 150, 20), "Mặt sau")) {
-			Application.LoadLevel (6);
+			
 		}
 		
 		//Bảng mach luồng E1
 		if (GUI.Button (new Rect (20, 330, 150, 20), "Bảng mạch luồng E1")) {
-			Application.LoadLevel (7);
+			
 		}
 		
 		//Bảng mach thuê bao
 		if (GUI.Button (new Rect (20, 355, 150, 20), "Bảng mạch thuê bao")) {
-			Application.LoadLevel (8);
+			
 		}
 		
 		//Bảng mach CO, ĐKX
 		if (GUI.Button (new Rect (20, 380, 150, 20), "Bảng mạch CO, ĐKX")) {
-			Application.LoadLevel (9);
+			
 		}
 		
 		//Bảng mạch nguồn
 		if (GUI.Button (new Rect (20, 405, 150, 20), "Bảng mạch nguồn")) {
-			Application.LoadLevel (10);
+			
 		}
 		#endregion
 		
@@ -92,17 +92,17 @@ public class menu : MonoBehaviour
 		
 		//Tháo lắp
 		if (GUI.Button (new Rect (20, 465, 150, 20), "Tháo / Lắp")) {
-			Application.LoadLevel (11);
+			
 		}
 		
 		//Đấu nối
 		if (GUI.Button (new Rect (20, 490, 150, 20), "Đấu nối")) {
-			Application.LoadLevel (12);
+			
 		}
 		
 		//Khai báo
 		if (GUI.Button (new Rect (20, 515, 150, 20), "Khai báo")) {
-			Application.LoadLevel (13);
+			
 		}
 		#endregion
 		
@@ -114,12 +114,12 @@ public class menu : MonoBehaviour
 		
 		//Khai báo
 		if (GUI.Button (new Rect (20, 575, 150, 20), "Hướng dẫn sử dụng")) {
-			Application.LoadLevel (14);
+			
 		}
 		
 		//Thoát
 		if (GUI.Button (new Rect (1250, 560, 100, 40), "Thoát")) {
-			Application.LoadLevel (14);
+			
 		}
 	}	
 }
